3c4c108c75790a076bd92a13e86cba09549f0ab6
hongkaiqiu <599182526@qq.com>
2025-07-02 19:51:12 +0800

    fix：change default theme

--- DodgeGhost/src/config/ThemeConfig.js
+++ DodgeGhost/src/config/ThemeConfig.js
@@ -1,4 +1,7 @@
-const theme = "demo";
+// const theme = "demo";
+// const theme = "FlappyBird";
+const theme = "TeamfightTactics";
+
 
 export const themeConfig = {
     theme: theme,
@@ -12,13 +15,32 @@ export const themeConfig = {
         path: `./assets/${theme}/player.png`,
         desc: "玩家静态图"
     },
+    // demo
+    // playerSpritesheet: {
+    //     key: "playerSpritesheet",
+    //     path: `./assets/${theme}/player-spritesheet.png`,
+    //     desc: "玩家动态精灵图",
+    //     frameWidth: 48,
+    //     frameHeight: 48,
+    //     totalFrames: 4
+    // },
+    // flappybird
+    // playerSpritesheet: {
+    //     key: "playerSpritesheet",
+    //     path: `./assets/${theme}/player-spritesheet.png`,
+    //     desc: "玩家动态精灵图",
+    //     frameWidth: 1024,
+    //     frameHeight: 1024,
+    //     totalFrames: 2
+    // },
+    // teamfighttactics
     playerSpritesheet: {
         key: "playerSpritesheet",
         path: `./assets/${theme}/player-spritesheet.png`,
         desc: "玩家动态精灵图",
-        frameWidth: 48,
-        frameHeight: 48,
-        totalFrames: 4
+        frameWidth: 640,
+        frameHeight: 640,
+        totalFrames: 24
     },
     ghost: {
         key: "ghost",
